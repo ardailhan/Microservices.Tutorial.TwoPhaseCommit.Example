ef4f3039835e8605dd6eac5d0b9b59ce6a719ee8
Arda İlhan <ardailhan7@gmail.com>
2024-02-02 14:45:14 +0300

    final configurations done and tested succesfully

--- Coordinator/Program.cs
+++ Coordinator/Program.cs
@@ -1,10 +1,11 @@
 using Coordinator.Models.Context;
+using Coordinator.Services;
+using Coordinator.Services.Abstraction;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TwoPhaseCommitContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer")));
@@ -12,6 +13,8 @@ builder.Services.AddDbContext<TwoPhaseCommitContext>(options => options.UseSqlSe
 builder.Services.AddHttpClient("OrderAPI", client => client.BaseAddress = new("https://localhost:7106/"));
 builder.Services.AddHttpClient("StockAPI", client => client.BaseAddress = new("https://localhost:7076/"));
 builder.Services.AddHttpClient("PaymentAPI", client => client.BaseAddress = new("https://localhost:7123/"));
+
+builder.Services.AddTransient<ITransactionService, TransactionService>();
 
 var app = builder.Build();
 
@@ -21,10 +24,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.MapGet("/create-order-transaction", async (ITransactionService transactionService) =>
+{
+    //Phase 1 - Prepare
+    var transactionId = await transactionService.CreateTransactionAsync();
+    await transactionService.PrepareServicesAsync(transactionId);
+    bool transactionState = await transactionService.CheckReadyServicesAsync(transactionId);
 
-app.MapControllers();
+    if (transactionState)
+    {
+        //Phase 2 - Commit
+        await transactionService.CommitAsync(transactionId);
+        transactionState = await transactionService.CheckTransactionStateServicesAsync(transactionId);
+    }
+    if (!transactionState)
+    {
+        await transactionService.RollbackAsync(transactionId);
+    }
+});
 
 app.Run();
