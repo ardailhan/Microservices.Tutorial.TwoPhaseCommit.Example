60739339fb6c44efb83bf480269f4481e4b4a990
Arda İlhan <ardailhan7@gmail.com>
2024-02-02 13:44:00 +0300

    Project upgraded to .Net 8

--- Coordinator/Services/TransactionService.cs
+++ Coordinator/Services/TransactionService.cs
@@ -1,16 +1,55 @@
-﻿using Coordinator.Services.Abstraction;
+﻿using Coordinator.Models;
+using Coordinator.Models.Context;
+using Coordinator.Services.Abstraction;
+using Microsoft.EntityFrameworkCore;
 
 namespace Coordinator.Services
 {
-    public class TransactionService : ITransactionService
+    public class TransactionService(IHttpClientFactory _httpClientFactory, TwoPhaseCommitContext _context) : ITransactionService
     {
-        public Task<Guid> CreateTransactionAsync()
+        
+        HttpClient _orderHttpClient = _httpClientFactory.CreateClient("OrderAPI");
+        HttpClient _stockHttpClient;
+        HttpClient _paymentHttpClient;
+
+        public async Task<Guid> CreateTransactionAsync()
         {
-            throw new NotImplementedException();
+            Guid transactionId = Guid.NewGuid();
+            var nodes = await _context.Nodes.ToListAsync();
+            nodes.ForEach(node => node.NodeStates = new List<NodeState>()
+            {
+                new(transactionId)
+                {
+                    IsReady = Enums.ReadyType.Pending,
+                    TransactionState = Enums.TransactionState.Pending
+                }
+            });
+            await _context.SaveChangesAsync();
+            return transactionId;
         }
-        public Task PrepareServicesAsync(Guid transactionId)
+        public async Task PrepareServicesAsync(Guid transactionId)
         {
-            throw new NotImplementedException();
+            var transactionNodes = await _context.NodeStates
+                .Include(ns => ns.Node)
+                .Where(ns => ns.TransactionId == transactionId)
+                .ToListAsync();
+            foreach (var transactionNode in transactionNodes)
+            {
+                try
+                {
+                    var response = transactionNode.Node.Name switch
+                    {
+                        "Order.API" => "...",
+                        "Stock.API" => "...",
+                        "Payment.API" => "...",
+                    };
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
         }
         public Task<bool> CheckReadyServicesAsync(Guid transactionId)
         {
